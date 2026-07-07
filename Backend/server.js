6ca90eeea3b19ea5f0e24238a5fa59c75ff5cf70
Tanuj <152773375+Codesman2023@users.noreply.github.com>
2026-07-07 20:55:47 +0530

    Prepare backend for render deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,11 @@
 const app = require('./src/app')
 const { initializeRecurringTransactions } = require('./src/services/recurring.service');
 require('dotenv').config();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, async ()=>{
-    console.log('server is running on port 3000')
+    console.log(`Server is running on port ${port}`)
     try {
         await initializeRecurringTransactions();
         console.log('Recurring transactions initialized');
